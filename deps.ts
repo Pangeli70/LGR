f319f338c54f22492be279eeb28f763b443c252a
Paolo Angeli <pangeli70@gmail.com>
2023-03-27 02:54:33 +0200

    Changed LogEnd logic.

--- deps.ts
+++ deps.ts
@@ -5,9 +5,10 @@
  */
 
 // https://deno.land/std
-export * as StdFs from "https://deno.land/std@0.153.0/fs/mod.ts";
-export * as StdPath from "https://deno.land/std@0.153.0/path/mod.ts";
+export * as StdFs from "https://deno.land/std@0.180.0/fs/mod.ts";
+export * as StdPath from "https://deno.land/std@0.180.0/path/mod.ts";
 
+// TODO @1 remove this and upgrade to https://deno.land/std@0.180.0/dotenv/mod.ts -- APG 20230318
 export * as DotEnv from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
 // https://deno.land/x/mongo
@@ -16,13 +17,13 @@ export {
     MongoClient,
     Database as MongoDatabase,
     Collection as MongoCollection
-} from "https://deno.land/x/mongo@v0.29.3/mod.ts";
+} from "https://deno.land/x/mongo@v0.31.1/mod.ts";
 
 export type {
     FindOptions as MongoFindOpts,
     CountOptions as MongoCountOpts, 
     ConnectOptions as MongoConnOpts
-} from "https://deno.land/x/mongo@v0.29.3/mod.ts";
+} from "https://deno.land/x/mongo@v0.31.1/mod.ts";
 
 //deno.land/x/web_bson
 export {
@@ -30,8 +31,11 @@ export {
 } from 'https://deno.land/x/web_bson@v0.1.10/src/bson.ts';
 
 // https://github
+
 export * as Uts from "https://raw.githubusercontent.com/Pangeli70/apg-uts/master/mod.ts";
 export * as Rst from "https://raw.githubusercontent.com/Pangeli70/apg-rst/master/mod.ts";
-//export * as Rst from "../RST/mod.ts";
 export * as Mng from "https://raw.githubusercontent.com/Pangeli70/apg-mng/master/mod.ts";
-//export * as Mng from "../MNG/mod.ts";
+
+// export * as Uts from "../apg-uts/mod.ts";
+// export * as Rst from "../apg-rst/mod.ts";
+// export * as Mng from "../apg-mng/mod.ts";
